e184a14c37f90ffb6f5423cc69896dbb70c77fe5
akirarian17 <akirariansatyadhamma@gmail.com>
2026-06-09 05:56:10 +0800

    asdasad

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -51,10 +51,10 @@ def train_model(
 
     predictions = model.predict(X_test)
 
-    mlflow.sklearn.log_model(
-        model,
-        "random_forest_model"
-    )
+    # mlflow.sklearn.log_model(
+    #     model,
+    #     "random_forest_model"
+    # )
     
     accuracy = accuracy_score(
         y_test,
